adb9f1ecf35599c5da5deb0fe3fbb13ef6329791
hoangnguyennn <nvanhoang188@gmail.com>
2025-04-20 10:03:31 +0700

    add calendar storybook

---
+++ src/components/calendar/index.stories.ts
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import HnCalendar from './calendar.vue'
+
+const meta: Meta<typeof HnCalendar> = {
+  title: 'HnCalendar',
+  component: HnCalendar
+}
+
+export default meta
+
+type Story = StoryObj<typeof HnCalendar>
+
+export const Cover: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: `<hn-calendar />`
+      }
+    }
+  }
+}
+
+export const MinDate: Story = {
+  args: {
+    minDate: new Date(2025, 0, 5) // 2025-01-05
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: `<hn-calendar :min-date="new Date(2025, 0, 5)" />`
+      }
+    }
+  }
+}
+
+export const MaxDate: Story = {
+  args: {
+    maxDate: new Date(2025, 0, 5) // 2025-01-05
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: `<hn-calendar :max-date="new Date(2025, 0, 5)" />`
+      }
+    }
+  }
+}
