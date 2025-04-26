aeb119b8191c8a269f03157f8952e4cb7253acc5
hoangnguyennn <hoangnguyennn@users.noreply.github.com>
2025-04-26 10:49:20 +0000

    deploy: 78556ff9690434a23d2d5e42bd1dc872a605ac4e

--- assets/index.stories-DUKE7C5B.js
+++ assets/index.stories-DUKE7C5B.js
@@ -1,4 +1,4 @@
-import"./vue.esm-bundler-_1pGZRBO.js";import{I as n}from"./ico_check-3bnMLOEa.js";import{_ as c}from"./icon-CD1jb4hD.js";const d={title:"HnIcon",component:c},e={args:{as:n},render:a=>({components:{HnIcon:c},setup(){return{args:a}},template:'<HnIcon v-bind="args" />'}),parameters:{docs:{source:{code:"<HnIcon as={IcoCheck} />"}}}};var r,o,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+import"./vue.esm-bundler-DTmWGxf7.js";import{I as n}from"./ico_check-C3QKH8FM.js";import{_ as c}from"./icon-BcCFNny9.js";const d={title:"HnIcon",component:c},e={args:{as:n},render:a=>({components:{HnIcon:c},setup(){return{args:a}},template:'<HnIcon v-bind="args" />'}),parameters:{docs:{source:{code:"<HnIcon as={IcoCheck} />"}}}};var r,o,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
   args: {
     as: IcoCheck
   },
