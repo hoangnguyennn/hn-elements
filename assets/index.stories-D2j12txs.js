ac0640ae38f59123813250b26bec36ff4cf60337
hoangnguyennn <hoangnguyennn@users.noreply.github.com>
2025-04-26 07:53:31 +0000

    deploy: d73f32c5306bcdb4d2121da0c2a4b9d79182afe1

---
+++ assets/index.stories-D2j12txs.js
@@ -0,0 +1,42 @@
+import{_ as v}from"./checkbox-BdDrZ9vK.js";import"./vue.esm-bundler-BeCrL08q.js";const E={title:"HnCheckbox",component:v,args:{label:"Label"}},o={parameters:{docs:{source:{code:"<hn-checkbox />"}}}},e={args:{disabled:!0},parameters:{docs:{source:{code:"<hn-checkbox disabled />"}}}},r={args:{error:!0},parameters:{docs:{source:{code:"<hn-checkbox error />"}}}},n={args:{indeterminate:!0},parameters:{docs:{source:{code:"<hn-checkbox indeterminate />"}}}};var s,c,a;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      source: {
+        code: \`<hn-checkbox />\`
+      }
+    }
+  }
+}`,...(a=(c=o.parameters)==null?void 0:c.docs)==null?void 0:a.source}}};var t,d,i,m,h;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: \`<hn-checkbox disabled />\`
+      }
+    }
+  }
+}`,...(i=(d=e.parameters)==null?void 0:d.docs)==null?void 0:i.source},description:{story:"Sử dụng thuộc tính `disabled` của `hn-checkbox` để vô hiệu hoá nó.",...(h=(m=e.parameters)==null?void 0:m.docs)==null?void 0:h.description}}};var p,u,b,g,l;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    error: true
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: \`<hn-checkbox error />\`
+      }
+    }
+  }
+}`,...(b=(u=r.parameters)==null?void 0:u.docs)==null?void 0:b.source},description:{story:"Sử dụng thuộc tính `error` của `hn-checkbox` để hiển thị trạng thái lỗi.",...(l=(g=r.parameters)==null?void 0:g.docs)==null?void 0:l.description}}};var x,k,S,_,f;n.parameters={...n.parameters,docs:{...(x=n.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    indeterminate: true
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: \`<hn-checkbox indeterminate />\`
+      }
+    }
+  }
+}`,...(S=(k=n.parameters)==null?void 0:k.docs)==null?void 0:S.source},description:{story:"Sử dụng thuộc tính `indeterminate` của `hn-checkbox` để hiển thị trạng thái\nkhông xác định.",...(f=(_=n.parameters)==null?void 0:_.docs)==null?void 0:f.description}}};const D=["Cover","Disabled","Error","Indeterminate"];export{o as Cover,e as Disabled,r as Error,n as Indeterminate,D as __namedExportsOrder,E as default};
---
+++ assets/preview-BomUW3tM.js
@@ -0,0 +1,1 @@
+const o={parameters:{controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},docs:{canvas:{sourceState:"shown"}}},tags:["autodocs"]};export{o as default};
