aa920dd64d01d33640881598889d196238150da5
Hoang Nguyen <nvanhoang188@gmail.com>
2025-04-26 17:30:20 +0700

    add radio storybook (#61)

---
+++ src/components/radio/radio-group.stories.ts
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { HnRadio, HnRadioGroup } from '.'
+
+const meta: Meta<typeof HnRadioGroup> = {
+  title: 'HnRadioGroup',
+  component: HnRadioGroup
+}
+
+export default meta
+
+type Story = StoryObj<typeof HnRadioGroup>
+
+export const Cover: Story = {
+  render: () => ({
+    components: { HnRadioGroup, HnRadio },
+    template: `<hn-radio-group>
+  <hn-radio label="Option 1" value="1" />
+  <hn-radio label="Option 2" value="2" />
+  <hn-radio label="Option 3" value="3" />
+</hn-radio-group>`
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: `<hn-radio-group>
+  <hn-radio label="Option 1" value="1" />
+  <hn-radio label="Option 2" value="2" />
+  <hn-radio label="Option 3" value="3" />
+</hn-radio-group>`
+      }
+    }
+  }
+}
+
+/** Sử dụng thuộc tính `direction` của `hn-radio-group` để thay đổi hướng của nó. */
+export const Direction: Story = {
+  render: () => ({
+    components: { HnRadioGroup, HnRadio },
+    template: `<hn-radio-group direction="horizontal">
+  <hn-radio label="Option 1" value="1" />
+  <hn-radio label="Option 2" value="2" />
+  <hn-radio label="Option 3" value="3" />
+</hn-radio-group>`
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: `<hn-radio-group direction="horizontal">
+  <hn-radio label="Option 1" value="1" />
+  <hn-radio label="Option 2" value="2" />
+  <hn-radio label="Option 3" value="3" />
+</hn-radio-group>`
+      }
+    }
+  }
+}
