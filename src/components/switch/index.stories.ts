ccfb97d2e6e42c44a264c5c67daa160b95a2e63b
hoangnguyennn <nvanhoang188@gmail.com>
2025-04-29 20:37:04 +0700

    add switch storybook

---
+++ src/components/switch/index.stories.ts
@@ -0,0 +1,52 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { HnSwitch } from '.'
+
+const meta: Meta<typeof HnSwitch> = {
+  title: 'HnSwitch',
+  component: HnSwitch
+}
+
+export default meta
+
+type Story = StoryObj<typeof HnSwitch>
+
+export const Cover: Story = {
+  parameters: {
+    docs: {
+      source: {
+        code: `<hn-switch />`
+      }
+    }
+  }
+}
+
+/**
+ * Sử dụng prop `show-label` của `hn-switch` để hiển thị label của trạng thái
+ * hiện tại.
+ */
+export const ShowLabel: Story = {
+  args: {
+    showLabel: true
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: `<hn-switch show-label />`
+      }
+    }
+  }
+}
+
+/** Sử dụng prop `disabled` của `hn-switch` để vô hiệu hoá nó. */
+export const Disabled: Story = {
+  args: {
+    disabled: true
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: `<hn-switch disabled />`
+      }
+    }
+  }
+}
