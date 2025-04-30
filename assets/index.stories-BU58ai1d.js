ff9aa0365bbab6a52eb6b5cd87ce104f5da34bfe
hoangnguyennn <hoangnguyennn@users.noreply.github.com>
2025-04-30 03:21:18 +0000

    deploy: b81ba2c5bcbf39055c03d2d576f86583774eda6b

---
+++ assets/index.stories-BU58ai1d.js
@@ -0,0 +1,23 @@
+import"./vue.esm-bundler-BFoVTCN0.js";import{I as n}from"./ico_check-DMF4eg3e.js";import{_ as c}from"./icon-A8lS6erG.js";const d={title:"HnIcon",component:c},e={args:{as:n},render:a=>({components:{HnIcon:c},setup(){return{args:a}},template:'<HnIcon v-bind="args" />'}),parameters:{docs:{source:{code:"<HnIcon as={IcoCheck} />"}}}};var r,o,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    as: IcoCheck
+  },
+  render: args => ({
+    components: {
+      HnIcon
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: '<HnIcon v-bind="args" />'
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: '<HnIcon as={IcoCheck} />'
+      }
+    }
+  }
+}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const I=["Cover"];export{e as Cover,I as __namedExportsOrder,d as default};
---
+++ assets/index.stories-BcbgXb9C.js
@@ -0,0 +1,42 @@
+import{_ as v}from"./checkbox-BSv83M9Z.js";import"./vue.esm-bundler-BFoVTCN0.js";import"./ico_check-DMF4eg3e.js";const D={title:"HnCheckbox",component:v,args:{label:"Label"}},o={parameters:{docs:{source:{code:"<hn-checkbox />"}}}},e={args:{disabled:!0},parameters:{docs:{source:{code:"<hn-checkbox disabled />"}}}},r={args:{error:!0},parameters:{docs:{source:{code:"<hn-checkbox error />"}}}},n={args:{indeterminate:!0},parameters:{docs:{source:{code:"<hn-checkbox indeterminate />"}}}};var s,c,a;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  parameters: {
+    docs: {
+      source: {
+        code: \`<hn-checkbox />\`
+      }
+    }
+  }
+}`,...(a=(c=o.parameters)==null?void 0:c.docs)==null?void 0:a.source}}};var t,d,i,m,h;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: \`<hn-checkbox disabled />\`
+      }
+    }
+  }
+}`,...(i=(d=e.parameters)==null?void 0:d.docs)==null?void 0:i.source},description:{story:"Sử dụng thuộc tính `disabled` của `hn-checkbox` để vô hiệu hoá nó.",...(h=(m=e.parameters)==null?void 0:m.docs)==null?void 0:h.description}}};var p,u,b,g,l;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    error: true
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: \`<hn-checkbox error />\`
+      }
+    }
+  }
+}`,...(b=(u=r.parameters)==null?void 0:u.docs)==null?void 0:b.source},description:{story:"Sử dụng thuộc tính `error` của `hn-checkbox` để hiển thị trạng thái lỗi.",...(l=(g=r.parameters)==null?void 0:g.docs)==null?void 0:l.description}}};var x,k,S,_,f;n.parameters={...n.parameters,docs:{...(x=n.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    indeterminate: true
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: \`<hn-checkbox indeterminate />\`
+      }
+    }
+  }
+}`,...(S=(k=n.parameters)==null?void 0:k.docs)==null?void 0:S.source},description:{story:"Sử dụng thuộc tính `indeterminate` của `hn-checkbox` để hiển thị trạng thái\nkhông xác định.",...(f=(_=n.parameters)==null?void 0:_.docs)==null?void 0:f.description}}};const I=["Cover","Disabled","Error","Indeterminate"];export{o as Cover,e as Disabled,r as Error,n as Indeterminate,I as __namedExportsOrder,D as default};
