f11bad2abe547e090f489cf69d11f5b70037e4b2
Hoang Nguyen <nvanhoang188@gmail.com>
2025-04-30 12:06:13 +0700

    create internal hn-calendar value (#73)

--- src/components/calendar/calendar.ts
+++ src/components/calendar/calendar.ts
@@ -1,13 +1,6 @@
-export type CalendarEmits = {
-  /** Kích hoạt khi thay đổi ngày. */
-  'update:modelValue': [CalendarValue]
-  /** Kích hoạt khi thay đổi ngày. */
-  change: [CalendarValue]
-}
+export type CalendarPane = 'day' | 'month' | 'year'
 
 export type CalendarValue = Date | undefined
-
-export type CalendarPane = 'day' | 'month' | 'year'
 
 export type CalendarProps = {
   /** Giá trị ngày được chọn hiện tại. */
@@ -16,4 +9,11 @@ export type CalendarProps = {
   minDate?: Date
   /** Giới hạn ngày lớn nhất. */
   maxDate?: Date
+}
+
+export type CalendarEmits = {
+  /** Kích hoạt khi thay đổi ngày. */
+  'update:modelValue': [CalendarValue]
+  /** Kích hoạt khi thay đổi ngày. */
+  change: [CalendarValue]
 }
