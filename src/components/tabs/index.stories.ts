b81ba2c5bcbf39055c03d2d576f86583774eda6b
Hoang Nguyen <nvanhoang188@gmail.com>
2025-04-30 10:20:50 +0700

    add tabs storybook (#70)

---
+++ src/components/tabs/index.stories.ts
@@ -0,0 +1,65 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+import { HnTab, HnTabGroup } from '.'
+
+const meta: Meta<typeof HnTabGroup> = {
+  title: 'HnTabs',
+  component: HnTabGroup
+}
+
+export default meta
+
+type Story = StoryObj<typeof HnTabGroup>
+
+export const Cover: Story = {
+  render: args => ({
+    components: { HnTab, HnTabGroup },
+    setup() {
+      return { args }
+    },
+    template: `<hn-tab-group v-bind="args">
+  <hn-tab>Tab 1</hn-tab>
+  <hn-tab>Tab 2</hn-tab>
+  <hn-tab>Tab 3</hn-tab>
+</hn-tab-group>`
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: `<hn-tab-group>
+  <hn-tab>Tab 1</hn-tab>
+  <hn-tab>Tab 2</hn-tab>
+  <hn-tab>Tab 3</hn-tab>
+</hn-tab-group>`
+      }
+    }
+  }
+}
+
+/** Sử dụng thuộc tính `direction` của `hn-tab`group` để đổi hướng của nó. */
+export const Direction: Story = {
+  args: {
+    direction: 'vertical'
+  },
+  render: args => ({
+    components: { HnTab, HnTabGroup },
+    setup() {
+      return { args }
+    },
+    template: `<hn-tab-group v-bind="args">
+  <hn-tab>Tab 1</hn-tab>
+  <hn-tab>Tab 2</hn-tab>
+  <hn-tab>Tab 3</hn-tab>
+</hn-tab-group>`
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: `<hn-tab-group direction="vertical">
+  <hn-tab>Tab 1</hn-tab>
+  <hn-tab>Tab 2</hn-tab>
+  <hn-tab>Tab 3</hn-tab>
+</hn-tab-group>`
+      }
+    }
+  }
+}
