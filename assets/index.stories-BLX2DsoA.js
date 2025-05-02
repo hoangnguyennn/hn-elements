62bfa43e8fd9048473120caa26f44ba328b09849
hoangnguyennn <hoangnguyennn@users.noreply.github.com>
2025-05-02 02:04:12 +0000

    deploy: 462b33995cf68f74bdcac5b2b97dd0b3d1ad9461

---
+++ assets/index.stories-BLX2DsoA.js
@@ -0,0 +1,12 @@
+import"./vue.esm-bundler-DcsGvaBf.js";import{I as c}from"./ico_check-CIzoPOy9.js";import{_ as n}from"./icon-button-BNGkgLU_.js";import"./icon-DuCXg-vs.js";const d={title:"HnIconButton",component:n},o={args:{as:c},parameters:{docs:{source:{code:"<HnIconButton as={IcoCheck} />"}}}};var e,r,s;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    as: IcoCheck
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: '<HnIconButton as={IcoCheck} />'
+      }
+    }
+  }
+}`,...(s=(r=o.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const u=["Cover"];export{o as Cover,u as __namedExportsOrder,d as default};
