48303098edc91f4639983c53cac0d9a1d3bde682
hoangnguyennn <hoangnguyennn@users.noreply.github.com>
2025-05-06 06:24:13 +0000

    deploy: d6bc3d212ba547f44a57e7a2083674e756c4cb6c

---
+++ assets/index.stories-mXLmxE_K.js
@@ -0,0 +1,12 @@
+import"./vue.esm-bundler-fMwKVOuU.js";import{I as c}from"./ico_check-C3pIuTVm.js";import{_ as a}from"./icon-CPqTj-aL.js";const p={title:"HnIcon",component:a},o={args:{as:c},parameters:{docs:{source:{code:"<hn-icon as={IcoCheck} />"}}}};var e,r,s;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    as: IcoCheck
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: '<hn-icon as={IcoCheck} />'
+      }
+    }
+  }
+}`,...(s=(r=o.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const d=["Cover"];export{o as Cover,d as __namedExportsOrder,p as default};
