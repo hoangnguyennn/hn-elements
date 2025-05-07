57c240cc110e8eff3c25e1d321d69f61545ee5d2
hoangnguyennn <nvanhoang188@gmail.com>
2025-05-07 22:26:43 +0700

    .

--- src/components/avatar/index.test.ts
+++ src/components/avatar/index.test.ts
@@ -5,25 +5,22 @@ import { HnAvatar, type AvatarSize } from '.'
 describe('avatar', () => {
   it('component render thành công', () => {
     const { container } = render(HnAvatar)
-    const rootElement = container.firstChild
-    expect(rootElement).toHaveClass('hn-avatar')
-    expect(rootElement).toBeInTheDocument()
+    expect(container.firstChild).toBeInTheDocument()
+    expect(container.firstChild).toHaveClass('hn-avatar')
   })
 
   describe('truyền props', () => {
     describe('size', () => {
       it('avatar hiển thị với kích thước mặc định là medium', () => {
         const { container } = render(HnAvatar)
-        const rootElement = container.firstChild
-        expect(rootElement).toHaveAttribute('data-size', 'medium')
+        expect(container.firstChild).toHaveAttribute('data-size', 'medium')
       })
 
       it.each<{ size: AvatarSize }>([{ size: 'small' }, { size: 'medium' }, { size: 'large' }])(
         'avatar hiển thị với kích thước $size khi truyền size là $size',
         ({ size }) => {
           const { container } = render(HnAvatar, { props: { size } })
-          const rootElement = container.firstChild
-          expect(rootElement).toHaveAttribute('data-size', size)
+          expect(container.firstChild).toHaveAttribute('data-size', size)
         }
       )
     })
@@ -32,30 +29,25 @@ describe('avatar', () => {
       it('nếu truyền src, avatar hiển thị với src', () => {
         const avatar = 'https://via.placeholder.com/150'
         render(HnAvatar, { props: { src: avatar } })
-        const image = screen.getByRole('img')
-        expect(image).toBeInTheDocument()
-        expect(image).toHaveAttribute('src', avatar)
+        expect(screen.getByRole('img')).toBeInTheDocument()
+        expect(screen.getByRole('img')).toHaveAttribute('src', avatar)
       })
 
       it('nếu truyền name, avatar hiển thị với name', () => {
         const name = 'Hoang Nguyen'
         render(HnAvatar, { props: { name } })
-        const image = screen.queryByRole('img')
-        expect(image).not.toBeInTheDocument()
-        const nameElement = screen.getByText(getAcronym(name))
-        expect(nameElement).toBeInTheDocument()
+        expect(screen.queryByRole('img')).not.toBeInTheDocument()
+        expect(screen.getByText(getAcronym(name))).toBeInTheDocument()
       })
 
       it('nếu truyền cả src và name thì src được sử dụng làm image và name là alt', () => {
         const avatar = 'https://via.placeholder.com/150'
         const name = 'Hoang Nguyen'
         render(HnAvatar, { props: { src: avatar, name } })
-        const image = screen.getByRole('img')
-        expect(image).toBeInTheDocument()
-        expect(image).toHaveAttribute('src', avatar)
-        expect(image).toHaveAttribute('alt', name)
-        const nameElement = screen.queryByText(getAcronym(name))
-        expect(nameElement).not.toBeInTheDocument()
+        expect(screen.getByRole('img')).toBeInTheDocument()
+        expect(screen.getByRole('img')).toHaveAttribute('src', avatar)
+        expect(screen.getByRole('img')).toHaveAttribute('alt', name)
+        expect(screen.queryByText(getAcronym(name))).not.toBeInTheDocument()
       })
     })
   })
