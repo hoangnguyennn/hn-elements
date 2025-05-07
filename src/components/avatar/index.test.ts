f205f36942cd957497c001a5a5d8292de59c786b
Hoang Nguyen <nvanhoang188@gmail.com>
2025-05-07 22:28:15 +0700

    refactor(avatar): using testing library for avatar test (#115)

--- src/components/avatar/index.test.ts
+++ src/components/avatar/index.test.ts
@@ -1,60 +1,53 @@
 import { getAcronym } from '@hn/utils/string'
-import { mount } from '@vue/test-utils'
-import { HnAvatar } from '.'
+import { render, screen } from '@testing-library/vue'
+import { HnAvatar, type AvatarSize } from '.'
 
 describe('avatar', () => {
   it('component render thành công', () => {
-    const wrapper = mount(HnAvatar)
-    expect(wrapper.classes()).toContain('hn-avatar')
-    expect(wrapper.exists()).toBe(true)
+    const { container } = render(HnAvatar)
+    expect(container.firstChild).toBeInTheDocument()
+    expect(container.firstChild).toHaveClass('hn-avatar')
   })
 
   describe('truyền props', () => {
     describe('size', () => {
       it('avatar hiển thị với kích thước mặc định là medium', () => {
-        const wrapper = mount(HnAvatar)
-        expect(wrapper.attributes('data-size')).toBe('medium')
+        const { container } = render(HnAvatar)
+        expect(container.firstChild).toHaveAttribute('data-size', 'medium')
       })
 
-      it('avatar hiển thị với kích thước small khi truyền size là small', () => {
-        const wrapper = mount(HnAvatar, { props: { size: 'small' } })
-        expect(wrapper.attributes('data-size')).toBe('small')
-      })
-
-      it('avatar hiển thị với kích thước medium khi truyền size là medium', () => {
-        const wrapper = mount(HnAvatar, { props: { size: 'medium' } })
-        expect(wrapper.attributes('data-size')).toBe('medium')
-      })
-
-      it('avatar hiển thị với kích thước large khi truyền size là large', () => {
-        const wrapper = mount(HnAvatar, { props: { size: 'large' } })
-        expect(wrapper.attributes('data-size')).toBe('large')
-      })
+      it.each<{ size: AvatarSize }>([{ size: 'small' }, { size: 'medium' }, { size: 'large' }])(
+        'avatar hiển thị với kích thước $size khi truyền size là $size',
+        ({ size }) => {
+          const { container } = render(HnAvatar, { props: { size } })
+          expect(container.firstChild).toHaveAttribute('data-size', size)
+        }
+      )
     })
 
     describe('src và name', () => {
       it('nếu truyền src, avatar hiển thị với src', () => {
         const avatar = 'https://via.placeholder.com/150'
-        const wrapper = mount(HnAvatar, { props: { src: avatar } })
-        expect(wrapper.find('.hn-avatar--image').exists()).toBe(true)
-        expect(wrapper.find('.hn-avatar--image').attributes('src')).toBe(avatar)
+        render(HnAvatar, { props: { src: avatar } })
+        expect(screen.getByRole('img')).toBeInTheDocument()
+        expect(screen.getByRole('img')).toHaveAttribute('src', avatar)
       })
 
       it('nếu truyền name, avatar hiển thị với name', () => {
         const name = 'Hoang Nguyen'
-        const wrapper = mount(HnAvatar, { props: { name } })
-        expect(wrapper.find('.hn-avatar--name').exists()).toBe(true)
-        expect(wrapper.find('.hn-avatar--name').text()).toBe(getAcronym(name))
+        render(HnAvatar, { props: { name } })
+        expect(screen.queryByRole('img')).not.toBeInTheDocument()
+        expect(screen.getByText(getAcronym(name))).toBeInTheDocument()
       })
 
       it('nếu truyền cả src và name thì src được sử dụng làm image và name là alt', () => {
         const avatar = 'https://via.placeholder.com/150'
         const name = 'Hoang Nguyen'
-        const wrapper = mount(HnAvatar, { props: { src: avatar, name } })
-        expect(wrapper.find('.hn-avatar--image').exists()).toBe(true)
-        expect(wrapper.find('.hn-avatar--image').attributes('src')).toBe(avatar)
-        expect(wrapper.find('.hn-avatar--image').attributes('alt')).toBe(name)
-        expect(wrapper.find('.hn-avatar--name').exists()).toBe(false)
+        render(HnAvatar, { props: { src: avatar, name } })
+        expect(screen.getByRole('img')).toBeInTheDocument()
+        expect(screen.getByRole('img')).toHaveAttribute('src', avatar)
+        expect(screen.getByRole('img')).toHaveAttribute('alt', name)
+        expect(screen.queryByText(getAcronym(name))).not.toBeInTheDocument()
       })
     })
   })
