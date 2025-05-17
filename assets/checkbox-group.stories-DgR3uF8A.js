ace6f7e509b1ef218b5ab13b25bc8ae11fa46e09
hoangnguyennn <hoangnguyennn@users.noreply.github.com>
2025-05-17 00:44:00 +0000

    deploy: b1383933aa7f6c0f3e0a6de8fa42f8787a2ed0b0

---
+++ assets/checkbox-group.stories-DgR3uF8A.js
@@ -0,0 +1,76 @@
+import{_ as c}from"./checkbox-group-BFop4uEL.js";import{_ as i}from"./checkbox-C_W-eLWn.js";import"./vue.esm-bundler-BMJOeYAP.js";import"./ico_check-HhgdyfRt.js";const g={title:"HnCheckboxGroup",component:c,args:{direction:"vertical"}},n={render:o=>({components:{HnCheckboxGroup:c,HnCheckbox:i},setup(){return{args:o}},template:`<hn-checkbox-group v-bind="args">
+  <hn-checkbox label="Option 1" value="1" />
+  <hn-checkbox label="Option 2" value="2" />
+  <hn-checkbox label="Option 3" value="3" />
+</hn-checkbox-group>`}),parameters:{docs:{source:{code:`<hn-checkbox-group>
+  <hn-checkbox label="Option 1" value="1" />
+  <hn-checkbox label="Option 2" value="2" />
+  <hn-checkbox label="Option 3" value="3" />
+</hn-checkbox-group>`}}}},e={args:{direction:"horizontal"},render:o=>({components:{HnCheckboxGroup:c,HnCheckbox:i},setup(){return{args:o}},template:`<hn-checkbox-group v-bind="args">
+  <hn-checkbox label="Option 1" value="1" />
+  <hn-checkbox label="Option 2" value="2" />
+  <hn-checkbox label="Option 3" value="3" />
+</hn-checkbox-group>`}),parameters:{docs:{source:{code:`<hn-checkbox-group direction="horizontal">
+  <hn-checkbox label="Option 1" value="1" />
+  <hn-checkbox label="Option 2" value="2" />
+  <hn-checkbox label="Option 3" value="3" />
+</hn-checkbox-group>`}}}};var r,a,h;n.parameters={...n.parameters,docs:{...(r=n.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      HnCheckboxGroup,
+      HnCheckbox
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<hn-checkbox-group v-bind="args">
+  <hn-checkbox label="Option 1" value="1" />
+  <hn-checkbox label="Option 2" value="2" />
+  <hn-checkbox label="Option 3" value="3" />
+</hn-checkbox-group>\`
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: \`<hn-checkbox-group>
+  <hn-checkbox label="Option 1" value="1" />
+  <hn-checkbox label="Option 2" value="2" />
+  <hn-checkbox label="Option 3" value="3" />
+</hn-checkbox-group>\`
+      }
+    }
+  }
+}`,...(h=(a=n.parameters)==null?void 0:a.docs)==null?void 0:h.source}}};var t,l,p,b,u;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    direction: 'horizontal'
+  },
+  render: args => ({
+    components: {
+      HnCheckboxGroup,
+      HnCheckbox
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`<hn-checkbox-group v-bind="args">
+  <hn-checkbox label="Option 1" value="1" />
+  <hn-checkbox label="Option 2" value="2" />
+  <hn-checkbox label="Option 3" value="3" />
+</hn-checkbox-group>\`
+  }),
+  parameters: {
+    docs: {
+      source: {
+        code: \`<hn-checkbox-group direction="horizontal">
+  <hn-checkbox label="Option 1" value="1" />
+  <hn-checkbox label="Option 2" value="2" />
+  <hn-checkbox label="Option 3" value="3" />
+</hn-checkbox-group>\`
+      }
+    }
+  }
+}`,...(p=(l=e.parameters)==null?void 0:l.docs)==null?void 0:p.source},description:{story:"Sử dụng thuộc tính `direction` của `hn-checkbox-group` để thay đổi hướng của nó.",...(u=(b=e.parameters)==null?void 0:b.docs)==null?void 0:u.description}}};const m=["Cover","Direction"];export{n as Cover,e as Direction,m as __namedExportsOrder,g as default};
