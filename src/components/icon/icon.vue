1c48a4f955d4bf4949246d430684d3bce1cb5c3b
Hoang Nguyen <nvanhoang188@gmail.com>
2025-08-11 08:57:15 +0700

    chore(icon): fix `as` prop type warning (#150)

--- src/components/icon/icon.vue
+++ src/components/icon/icon.vue
@@ -1,18 +1,26 @@
 <template>
-  <component :is="as" class="hn-icon" :aria-label="ariaLabel" :aria-hidden="ariaHidden" :role="role"></component>
+  <component
+    :is="computedAs"
+    class="hn-icon"
+    :aria-label="ariaLabel"
+    :aria-hidden="ariaHidden"
+    :role="role"
+  ></component>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, toRaw } from 'vue'
 import type { IconProps } from './icon'
 
 defineOptions({ name: 'HnIcon' })
 
-// TODO: ở dạng object, `as` đang bị dính warning về reactive
 const props = withDefaults(defineProps<IconProps>(), {
   ariaHidden: true
 })
 
+// NOTE: ở dạng object, `as` đang bị dính warning về reactive
+// Vì vậy, tôi sử dụng `toRaw` để lấy ra giá trị thực tế của `as`
+const computedAs = computed(() => (typeof props.as === 'string' ? props.as : toRaw(props.as)))
 const ariaHidden = computed(() => props.ariaHidden ?? !props.ariaLabel)
 const role = computed(() => props.role || (props.ariaLabel ? 'img' : undefined))
 </script>
