b0f077376b6588292be182cffa5cdc90ad14c67f
hoangnguyennn <nvanhoang188@gmail.com>
2025-08-11 08:50:26 +0700

    chore(icon): fix `as` prop type warning

--- src/components/icon/icon.ts
+++ src/components/icon/icon.ts
@@ -1,8 +1,8 @@
-import type { VNode } from 'vue'
+import type { Component } from 'vue'
 
 export type IconProps = {
   /** Chỉ định component được sử dụng trong icon. */
-  as: VNode | string
+  as: string | Component
   /** Nhãn aria cho icon. */
   ariaLabel: string
   /** Chỉ ra rằng icon có ẩn khỏi screen reader hay không. */
